c887f9b2fb315a12ebb426c35719cc648aaf5a53
mai <amaomruhki@gmail.com>
2022-10-09 20:44:00 +0900

    #21-Add_Function_DetailPageにコメント追加機能

--- pages/detail.tsx
+++ pages/detail.tsx
@@ -1,153 +1,291 @@
-import React, { useState } from 'react'
-import Link from 'next/link';
+import React, { useState, useEffect } from "react";
+import { db } from "../firebase/firebase";
 import {
-  Box,
-  Button,
-  Flex,
-  FormControl,
-  FormLabel,
-  Heading,
-  Input,
-  Modal,
-  ModalBody,
-  ModalCloseButton,
-  ModalContent,
-  ModalFooter,
-  ModalHeader,
-  ModalOverlay,
-  Spacer,
-  Text,
-  Textarea,
-  useDisclosure
-} from '@chakra-ui/react';
-import { EditIcon } from '@chakra-ui/icons';
-import styles from '../styles/Detail.module.css'
+	addDoc,
+	collection,
+	onSnapshot,
+	orderBy,
+	query,
+	serverTimestamp,
+} from "firebase/firestore";
+// import { useRecoilState } from "recoil";
+// import { userState } from "../atom/userAtom";
+//↓userAtomを作成し、以下を記述
+// import { atom } from "recoil";
+
+// export const userState = atom({
+//   key: "userState",
+//   default: null,
+// });
+import Link from "next/link";
+import { useRouter } from "next/router";
+import {
+	Box,
+	Button,
+	Flex,
+	FormControl,
+	FormLabel,
+	Heading,
+	Input,
+	Modal,
+	ModalBody,
+	ModalCloseButton,
+	ModalContent,
+	ModalFooter,
+	ModalHeader,
+	ModalOverlay,
+	Spacer,
+	Text,
+	Textarea,
+	useDisclosure,
+} from "@chakra-ui/react";
+import { EditIcon } from "@chakra-ui/icons";
+import styles from "../styles/Detail.module.css";
 
 const Detail = () => {
-  // sample
-  const [comments, useComments] = useState([
-    {name: 'じょん', text: 'Test Test Test Test Test Test Test', create_date: '2022/10/1', update_date: '2022/11/1'},
-    {name: 'じょん', text: 'Test Test Test Test Test Test Test', create_date: '2022/11/1', update_date: '2022/11/1'},
-    {name: 'じょん', text: 'Test Test Test Test Test Test Test', create_date: '2022/12/1', update_date: '2022/11/1'},
-    {name: 'じょん', text: 'Test Test Test Test Test Test Test', create_date: '2022/10/1', update_date: '2022/11/1'},
-    {name: 'じょん', text: 'Test Test Test Test Test Test Test', create_date: '2022/11/1', update_date: '2022/11/1'},
-    {name: 'じょん', text: 'Test Test Test Test Test Test Test', create_date: '2022/12/1', update_date: '2022/11/1'},
-    {name: 'じょん', text: 'Test Test Test Test Test Test Test', create_date: '2022/10/1', update_date: '2022/11/1'},
-    {name: 'じょん', text: 'Test Test Test Test Test Test Test', create_date: '2022/11/1', update_date: '2022/11/1'}
-  ])
+	const [comment, setComment] = useState("");
+	const [comments, setComments] = useState([]);
+	const router = useRouter();
+	const { id } = router.query;
+	// const [currentUser] = useRecoilState(userState)
 
-  const { isOpen, onOpen, onClose } = useDisclosure()
+	// コメントの取得
+	useEffect(() => {
+		const unsubscribe = onSnapshot(
+			query(
+				// idはtodo.idをuse.routerで取得
+				//collection"todos"のサブコレとして"comments"を作る想定
+				collection(db, "todos", id as string, "comments"),
+				orderBy("timestamp", "desc")
+			),
+			(snapshot) => {
+				//いったん型エラー回避のためany
+				setComments(snapshot.docs as any);
+			}
+		);
+		return () => unsubscribe();
+	}, [db, id]);
 
-  return (
-    <Box>
-      <Heading bg='green.300' py={{ base: 2 }} px={{ base: 4 }}>
-        TODO
-      </Heading>
+	//コメントの投稿
+	async function sendComment(event: React.MouseEvent<HTMLButtonElement>) {
+		event.preventDefault();
+		const commentToSend = comment;
+		setComment("");
+		await addDoc(collection(db, "todos", id as string, "comments"), {
+			comment: commentToSend,
+			//headerでログインログアウトを判断機能を追加し、recoilでグローバル状態管理する
+			username: currentUser?.username,
+			timestamp: serverTimestamp(),
+		});
+	}
 
-      <Box py={{ base: 6 }} px={{ base: 6 }}>
-        <Flex justifyContent='space-between'>
-          <Text className={styles.title}>Show Todo</Text>
-          <Box className={styles.button_box}>
-            <Button size='sm' onClick={onOpen}>Comment</Button>
-            <Modal isOpen={isOpen} onClose={onClose}>
-              <ModalOverlay />
-              <FormControl>
-                <ModalContent top='20%'>
-                  <ModalHeader>Comment</ModalHeader>
-                  <ModalCloseButton />
-                  <ModalBody>
-                    <Box>
-                      <FormLabel fontWeight='bold'>Name</FormLabel>
-                      <Input type='text' />
-                    </Box>
+	// // sample
+	// const [comments, useComments] = useState([
+	// 	{
+	// 		name: "じょん",
+	// 		text: "Test Test Test Test Test Test Test",
+	// 		create_date: "2022/10/1",
+	// 		update_date: "2022/11/1",
+	// 	},
+	// 	{
+	// 		name: "じょん",
+	// 		text: "Test Test Test Test Test Test Test",
+	// 		create_date: "2022/11/1",
+	// 		update_date: "2022/11/1",
+	// 	},
+	// 	{
+	// 		name: "じょん",
+	// 		text: "Test Test Test Test Test Test Test",
+	// 		create_date: "2022/12/1",
+	// 		update_date: "2022/11/1",
+	// 	},
+	// 	{
+	// 		name: "じょん",
+	// 		text: "Test Test Test Test Test Test Test",
+	// 		create_date: "2022/10/1",
+	// 		update_date: "2022/11/1",
+	// 	},
+	// 	{
+	// 		name: "じょん",
+	// 		text: "Test Test Test Test Test Test Test",
+	// 		create_date: "2022/11/1",
+	// 		update_date: "2022/11/1",
+	// 	},
+	// 	{
+	// 		name: "じょん",
+	// 		text: "Test Test Test Test Test Test Test",
+	// 		create_date: "2022/12/1",
+	// 		update_date: "2022/11/1",
+	// 	},
+	// 	{
+	// 		name: "じょん",
+	// 		text: "Test Test Test Test Test Test Test",
+	// 		create_date: "2022/10/1",
+	// 		update_date: "2022/11/1",
+	// 	},
+	// 	{
+	// 		name: "じょん",
+	// 		text: "Test Test Test Test Test Test Test",
+	// 		create_date: "2022/11/1",
+	// 		update_date: "2022/11/1",
+	// 	},
+	// ]);
 
-                    <Box mt='5'>
-                      <FormLabel fontWeight='bold'>Comment</FormLabel>
-                      <Textarea minH='200px' maxH='300px'/>
-                    </Box>
-                  </ModalBody>
+	const { isOpen, onOpen, onClose } = useDisclosure();
 
-                  <ModalFooter>
-                    <Button colorScheme='blue' color='white'>
-                      Create
-                    </Button>
-                    <Button colorScheme='red' mr='3' ml='10' onClick={onClose}>
-                      Close
-                    </Button>
-                  </ModalFooter>
-                </ModalContent>
-              </FormControl>
-            </Modal>
+	return (
+		<Box>
+			<Heading bg="green.300" py={{ base: 2 }} px={{ base: 4 }}>
+				TODO
+			</Heading>
 
-            <Link href='/'>
-              <Button className={styles.back_button} size='sm'>Back</Button>
-            </Link>
-          </Box>
-        </Flex>
+			<Box py={{ base: 6 }} px={{ base: 6 }}>
+				<Flex justifyContent="space-between">
+					<Text className={styles.title}>Show Todo</Text>
+					<Box className={styles.button_box}>
+						<Button size="sm" onClick={onOpen}>
+							Comment
+						</Button>
+						<Modal isOpen={isOpen} onClose={onClose}>
+							<ModalOverlay />
+							<FormControl>
+								<ModalContent top="20%">
+									<ModalHeader>Comment</ModalHeader>
+									<ModalCloseButton />
+									<ModalBody>
+										<Box>
+											<FormLabel fontWeight="bold">Name</FormLabel>
+											<Input type="text" />
+										</Box>
 
-        <Box className={styles.box} my={{ base: 6 }}>
-          <Box className={styles.detail_box} h='660px' p='5' border='1px' borderRadius='3xl'>
-            <Box>
-              <Heading pl='20px' bg='green.300'>Title</Heading>
-              <Box p='10px 20px 0' minH='50px' maxH='80px' bg='white' overflow='scroll'>
-                Test Test Test Test Test Test Test Test Test Test Test Test Test Test 
-                Test Test Test Test Test Test Test Test Test Test Test Test Test Test 
-              </Box>
-            </Box>
+										<Box mt="5">
+											<FormLabel fontWeight="bold">Comment</FormLabel>
+											<Textarea minH="200px" maxH="300px" />
+										</Box>
+									</ModalBody>
 
-            <Box mt='10'>
-              <Heading pl='20px' bg='green.300'>Detail</Heading>
-              <Box p='5' minH='360px' maxH='360px' bg='white' overflow='scroll'>
-                Test Test Test Test Test Test Test Test Test Test Test Test Test Test 
-                Test Test Test Test Test Test Test Test Test Test Test Test Test Test 
-                Test Test Test Test Test Test Test Test Test Test Test Test Test Test 
-                Test Test Test Test Test Test Test Test Test Test Test Test Test Test 
-                Test Test Test Test Test Test Test Test Test Test Test Test Test Test 
-                Test Test Test Test Test Test Test Test Test Test Test Test Test Test
-                Test Test Test Test Test Test Test Test Test Test Test Test Test Test 
-                Test Test Test Test Test Test Test Test Test Test Test Test Test Test 
-                Test Test Test Test Test Test Test Test Test Test Test Test Test Test 
-                Test Test Test Test Test Test Test Test Test Test Test Test Test Test 
-              </Box>
-            </Box>
-            <Flex alignItems='center' mt='10px' p='0 20px'>
-              <Button color='black' p='0 30px' size='sm' colorScheme='whatsapp' borderRadius='20px'>
-                Edit
-                <EditIcon ml='3'/>
-              </Button>
-              <Spacer />
-              <Box className={styles.time} fontWeight='bold'>
-                create
-                <br/>
-                2022/10/1 19:00
-              </Box>
-              <Spacer />
-              <Box className={styles.time} fontWeight='bold'>
-                update
-                <br/>
-                2022/10/1 20:00
-              </Box>
-            </Flex>
-          </Box>
+									<ModalFooter>
+										<Button
+											colorScheme="blue"
+											color="white"
+											onClick={sendComment}
+											disabled={!comment.trim()}
+										>
+											Create
+										</Button>
+										<Button colorScheme="red" mr="3" ml="10" onClick={onClose}>
+											Close
+										</Button>
+									</ModalFooter>
+								</ModalContent>
+							</FormControl>
+						</Modal>
 
-          <Box className={styles.comment_box} h='660px' p='5' overflow='scroll'>
-            <Text fontWeight='bold' bg='#f5f5f5' p='2'>Comment</Text>
-            {comments?.map((comment, index) => (
-              <Box mt='5' border='1px' borderRadius='20px' overflow='hidden' key={index}>
-                <Flex justifyContent='space-between' bg='green.300'>
-                  <Text fontWeight='bold' ml='5'>{comment.name}</Text>
-                  <Text mr='5'>{comment.create_date}</Text>
-                </Flex>
-                <Box p='10px 20px 0' minH='20px' bg='white'>
-                  {comment.text} 
-                </Box>
-              </Box>
-            ))}
-          </Box>
-        </Box>
-      </Box>
-    </Box>
-  )
-}
+						<Link href="/">
+							<Button className={styles.back_button} size="sm">
+								Back
+							</Button>
+						</Link>
+					</Box>
+				</Flex>
 
-export default Detail
+				<Box className={styles.box} my={{ base: 6 }}>
+					<Box
+						className={styles.detail_box}
+						h="660px"
+						p="5"
+						border="1px"
+						borderRadius="3xl"
+					>
+						<Box>
+							<Heading pl="20px" bg="green.300">
+								Title
+							</Heading>
+							<Box
+								p="10px 20px 0"
+								minH="50px"
+								maxH="80px"
+								bg="white"
+								overflow="scroll"
+							>
+								Test Test Test Test Test Test Test Test Test Test Test Test Test
+								Test Test Test Test Test Test Test Test Test Test Test Test Test
+								Test Test
+							</Box>
+						</Box>
+
+						<Box mt="10">
+							<Heading pl="20px" bg="green.300">
+								Detail
+							</Heading>
+							<Box p="5" minH="360px" maxH="360px" bg="white" overflow="scroll">
+								Test Test Test Test Test Test Test Test Test Test Test Test Test
+								Test Test Test Test Test Test Test Test Test Test Test Test Test
+								Test Test Test Test Test Test Test Test Test Test Test Test Test
+								Test Test Test Test Test Test Test Test Test Test Test Test Test
+								Test Test Test Test Test Test Test Test Test Test Test Test Test
+								Test Test Test Test Test Test Test Test Test Test Test Test Test
+								Test Test Test Test Test Test Test Test Test Test Test Test Test
+								Test Test Test Test Test Test Test Test Test Test Test Test Test
+								Test Test Test Test Test Test Test Test Test Test Test Test Test
+								Test Test Test Test Test Test Test Test Test Test Test Test Test
+								Test Test Test Test Test Test Test Test Test Test
+							</Box>
+						</Box>
+						<Flex alignItems="center" mt="10px" p="0 20px">
+							<Button
+								color="black"
+								p="0 30px"
+								size="sm"
+								colorScheme="whatsapp"
+								borderRadius="20px"
+							>
+								Edit
+								<EditIcon ml="3" />
+							</Button>
+							<Spacer />
+							<Box className={styles.time} fontWeight="bold">
+								create
+								<br />
+								2022/10/1 19:00
+							</Box>
+							<Spacer />
+							<Box className={styles.time} fontWeight="bold">
+								update
+								<br />
+								2022/10/1 20:00
+							</Box>
+						</Flex>
+					</Box>
+
+					<Box className={styles.comment_box} h="660px" p="5" overflow="scroll">
+						<Text fontWeight="bold" bg="#f5f5f5" p="2">
+							Comment
+						</Text>
+						{comments?.map((comment) => (
+							<Box
+								mt="5"
+								border="1px"
+								borderRadius="20px"
+								overflow="hidden"
+								key={comments.data().id}
+							>
+								<Flex justifyContent="space-between" bg="green.300">
+									<Text fontWeight="bold" ml="5">
+										{comment.data().username}
+									</Text>
+									<Text mr="5">{comment.data().timestamp?.toDate()}</Text>
+								</Flex>
+								<Box p="10px 20px 0" minH="20px" bg="white">
+									{comment.data().comment}
+								</Box>
+							</Box>
+						))}
+					</Box>
+				</Box>
+			</Box>
+		</Box>
+	);
+};
+
+export default Detail;
--- pages/top.tsx
+++ pages/top.tsx
@@ -1,251 +1,258 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
-  Box,
-  Container,
-  Flex,
-  Stack,
-  HStack,
-  Button,
-  Input,
-  Spacer,
-  Text,
-  Image,
-  Select,
-  TableContainer,
-  Table,
-  Thead,
-  Tbody,
-  Tr,
-  Th,
-  Td,
-} from '@chakra-ui/react';
-import {useRouter} from 'next/router';
+	Box,
+	Container,
+	Flex,
+	Stack,
+	HStack,
+	Button,
+	Input,
+	Spacer,
+	Text,
+	Image,
+	Select,
+	TableContainer,
+	Table,
+	Thead,
+	Tbody,
+	Tr,
+	Th,
+	Td,
+} from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import Link from "next/link";
 
 const top = () => {
-  const router = useRouter();
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      task: 'testttttttttttt',
-      status: 'DONE',
-      priority: 'High',
-      create_date: '2020-11-8 18:55',
-      update_date: '2020-11-8 18:55',
-    },
-    {
-      id: 2,
-      task: 'test2',
-      status: 'DOING',
-      priority: 'High',
-      create_date: '2020-11-8 18:55',
-      update_date: '2020-11-8 18:55',
-    },
-    {
-      id: 3,
-      task: 'test3',
-      status: 'NOT STARTED',
-      priority: 'High',
-      create_date: '2020-11-8 18:55',
-      update_date: '2020-11-8 18:55',
-    },
-  ]);
+	const router = useRouter();
+	const [todos, setTodos] = useState([
+		{
+			id: 1,
+			task: "testttttttttttt",
+			status: "DONE",
+			priority: "High",
+			create_date: "2020-11-8 18:55",
+			update_date: "2020-11-8 18:55",
+		},
+		{
+			id: 2,
+			task: "test2",
+			status: "DOING",
+			priority: "High",
+			create_date: "2020-11-8 18:55",
+			update_date: "2020-11-8 18:55",
+		},
+		{
+			id: 3,
+			task: "test3",
+			status: "NOT STARTED",
+			priority: "High",
+			create_date: "2020-11-8 18:55",
+			update_date: "2020-11-8 18:55",
+		},
+	]);
 
-  return (
-    <>
-      <Container p="20px 100px 0" w="100%" maxW="1080px">
-        <Box pb="15px">
-          <Text
-            fontSize="28px"
-            fontWeight="700"
-            color="blackAlpha.800"
-            lineHeight="33px"
-          >
-            TODO LIST
-          </Text>
-        </Box>
-        <Flex alignItems="flex-start">
-          <HStack w="600px" spacing="24px" alignItems="flex-end">
-            <Stack sx={filterBox}>
-              <Text sx={filterTitle}>SEARCH</Text>
-              <Input placeholder="Text" />
-            </Stack>
-            <Stack sx={filterBox}>
-              <Text sx={filterTitle}>STATUS</Text>
-              <Select placeholder="- - - - - - -">
-                <option value="option1">Not Starting</option>
-                <option value="option2">Doing</option>
-                <option value="option3">Done</option>
-              </Select>
-            </Stack>
-            <Stack sx={filterBox}>
-              <Text sx={filterTitle}>PRIORITY</Text>
-              <Select placeholder="- - - - - - -">
-                <option value="option1">High</option>
-                <option value="option2">Middle</option>
-                <option value="option3">Low</option>
-              </Select>
-            </Stack>
-            <Button
-              type="button"
-              color="black"
-              bg="blackAlpha.500"
-              borderColor="blackAlpha.800"
-              p="0 50px"
-              size="md"
-              borderRadius="50px"
-            >
-              RESET
-            </Button>
-          </HStack>
-          <Spacer />
-          <HStack spacing="16px">
-            <Image src="Trash Icon Button.png" />
-            <Image src="Draft Icon Button.png" />
-            <Image onClick={() => router.push("/create")} src="New Icon Button.png" _hover={{ opacity:0.8, cursor:"pointer" }}/>
-          </HStack>
-        </Flex>
-        <TableContainer w="100%" m="33px 0 16px">
-          <Table>
-            <Thead h="56px" bg="green.300">
-              <Tr>
-                <Th
-                  fontSize="24px"
-                  color="blackAlpha.800"
-                  textTransform="none"
-                  textAlign="left"
-                  p="0 0 0 10px"
-                  minW="100px"
-                >
-                  Task
-                </Th>
-                <Th
-                  fontSize="24px"
-                  color="blackAlpha.800"
-                  textTransform="none"
-                  textAlign="center"
-                  p="0"
-                  minW="100px"
-                >
-                  Status
-                </Th>
-                <Th
-                  fontSize="24px"
-                  color="blackAlpha.800"
-                  textTransform="none"
-                  textAlign="center"
-                  p="0"
-                  minW="100px"
-                >
-                  Priority
-                </Th>
-                <Th
-                  fontSize="24px"
-                  color="blackAlpha.800"
-                  textTransform="none"
-                  textAlign="center"
-                  p="0"
-                  minW="100px"
-                >
-                  Create
-                </Th>
-                <Th
-                  fontSize="24px"
-                  color="blackAlpha.800"
-                  textTransform="none"
-                  textAlign="center"
-                  p="0"
-                  minW="100px"
-                >
-                  Update
-                </Th>
-                <Th
-                  fontSize="24px"
-                  color="blackAlpha.800"
-                  textTransform="none"
-                  textAlign="center"
-                  p="0"
-                  minW="100px"
-                >
-                  Action
-                </Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              {todos.map((todo) => {
-                return (
-                  <Tr key={todo.id}>
-                    <Td textAlign="left" pl="10px">
-                      {todo.task}
-                    </Td>
-                    <Td textAlign="center">
-                      <Box
-                        w="120px"
-                        h="40px"
-                        lineHeight="40px"
-                        borderRadius="50px"
-                        bg="green.600"
-                        color="#F0FFF4"
-                      >
-                        <Text>{todo.status}</Text>
-                      </Box>
-                    </Td>
-                    <Td textAlign="center">
-                      <Select value={todo.priority}>
-                        <option value="option1">High</option>
-                        <option value="option2">Middle</option>
-                        <option value="option3">Low</option>
-                      </Select>
-                    </Td>
-                    <Td fontSize="14px" textAlign="center">
-                      {todo.create_date}
-                    </Td>
-                    <Td fontSize="14px" textAlign="center">
-                      {todo.update_date}
-                    </Td>
-                    <Td>
-                      <HStack spacing="16px" justify="center">
-                        <Image src="Edit.png" />
-                        <Image src="Trash.png" />
-                      </HStack>
-                    </Td>
-                  </Tr>
-                );
-              })}
-            </Tbody>
-          </Table>
-        </TableContainer>
-        <HStack justify="center" align="center">
-          <Box sx={pagenation}>＜</Box>
-          <Box sx={pagenation}>1</Box>
-          <Box sx={pagenation}>2</Box>
-          <Box sx={pagenation}>...</Box>
-          <Box sx={pagenation}>5</Box>
-          <Box sx={pagenation}>6</Box>
-          <Box sx={pagenation}>＞</Box>
-        </HStack>
-      </Container>
-    </>
-  );
+	return (
+		<>
+			<Container p="20px 100px 0" w="100%" maxW="1080px">
+				<Box pb="15px">
+					<Text
+						fontSize="28px"
+						fontWeight="700"
+						color="blackAlpha.800"
+						lineHeight="33px"
+					>
+						TODO LIST
+					</Text>
+				</Box>
+				<Flex alignItems="flex-start">
+					<HStack w="600px" spacing="24px" alignItems="flex-end">
+						<Stack sx={filterBox}>
+							<Text sx={filterTitle}>SEARCH</Text>
+							<Input placeholder="Text" />
+						</Stack>
+						<Stack sx={filterBox}>
+							<Text sx={filterTitle}>STATUS</Text>
+							<Select placeholder="- - - - - - -">
+								<option value="option1">Not Starting</option>
+								<option value="option2">Doing</option>
+								<option value="option3">Done</option>
+							</Select>
+						</Stack>
+						<Stack sx={filterBox}>
+							<Text sx={filterTitle}>PRIORITY</Text>
+							<Select placeholder="- - - - - - -">
+								<option value="option1">High</option>
+								<option value="option2">Middle</option>
+								<option value="option3">Low</option>
+							</Select>
+						</Stack>
+						<Button
+							type="button"
+							color="black"
+							bg="blackAlpha.500"
+							borderColor="blackAlpha.800"
+							p="0 50px"
+							size="md"
+							borderRadius="50px"
+						>
+							RESET
+						</Button>
+					</HStack>
+					<Spacer />
+					<HStack spacing="16px">
+						<Image src="Trash Icon Button.png" />
+						<Image src="Draft Icon Button.png" />
+						<Image
+							onClick={() => router.push("/create")}
+							src="New Icon Button.png"
+							_hover={{ opacity: 0.8, cursor: "pointer" }}
+						/>
+					</HStack>
+				</Flex>
+				<TableContainer w="100%" m="33px 0 16px">
+					<Table>
+						<Thead h="56px" bg="green.300">
+							<Tr>
+								<Th
+									fontSize="24px"
+									color="blackAlpha.800"
+									textTransform="none"
+									textAlign="left"
+									p="0 0 0 10px"
+									minW="100px"
+								>
+									Task
+								</Th>
+								<Th
+									fontSize="24px"
+									color="blackAlpha.800"
+									textTransform="none"
+									textAlign="center"
+									p="0"
+									minW="100px"
+								>
+									Status
+								</Th>
+								<Th
+									fontSize="24px"
+									color="blackAlpha.800"
+									textTransform="none"
+									textAlign="center"
+									p="0"
+									minW="100px"
+								>
+									Priority
+								</Th>
+								<Th
+									fontSize="24px"
+									color="blackAlpha.800"
+									textTransform="none"
+									textAlign="center"
+									p="0"
+									minW="100px"
+								>
+									Create
+								</Th>
+								<Th
+									fontSize="24px"
+									color="blackAlpha.800"
+									textTransform="none"
+									textAlign="center"
+									p="0"
+									minW="100px"
+								>
+									Update
+								</Th>
+								<Th
+									fontSize="24px"
+									color="blackAlpha.800"
+									textTransform="none"
+									textAlign="center"
+									p="0"
+									minW="100px"
+								>
+									Action
+								</Th>
+							</Tr>
+						</Thead>
+						<Tbody>
+							{todos.map((todo) => {
+								return (
+									<Tr key={todo.id}>
+										<Td textAlign="left" pl="10px">
+											<Link href={`/detail?id=${todo.id}`}>
+												<a>{todo.task}</a>
+											</Link>
+										</Td>
+										<Td textAlign="center">
+											<Box
+												w="120px"
+												h="40px"
+												lineHeight="40px"
+												borderRadius="50px"
+												bg="green.600"
+												color="#F0FFF4"
+											>
+												<Text>{todo.status}</Text>
+											</Box>
+										</Td>
+										<Td textAlign="center">
+											<Select value={todo.priority}>
+												<option value="option1">High</option>
+												<option value="option2">Middle</option>
+												<option value="option3">Low</option>
+											</Select>
+										</Td>
+										<Td fontSize="14px" textAlign="center">
+											{todo.create_date}
+										</Td>
+										<Td fontSize="14px" textAlign="center">
+											{todo.update_date}
+										</Td>
+										<Td>
+											<HStack spacing="16px" justify="center">
+												<Image src="Edit.png" />
+												<Image src="Trash.png" />
+											</HStack>
+										</Td>
+									</Tr>
+								);
+							})}
+						</Tbody>
+					</Table>
+				</TableContainer>
+				<HStack justify="center" align="center">
+					<Box sx={pagenation}>＜</Box>
+					<Box sx={pagenation}>1</Box>
+					<Box sx={pagenation}>2</Box>
+					<Box sx={pagenation}>...</Box>
+					<Box sx={pagenation}>5</Box>
+					<Box sx={pagenation}>6</Box>
+					<Box sx={pagenation}>＞</Box>
+				</HStack>
+			</Container>
+		</>
+	);
 };
 
 const filterBox = {
-  w: '100%',
-  minW: '120px',
+	w: "100%",
+	minW: "120px",
 };
 const filterTitle = {
-  fontWeight: '700',
-  fontSize: '18px',
-  lineHeight: '22px',
+	fontWeight: "700",
+	fontSize: "18px",
+	lineHeight: "22px",
 };
 const pagenation = {
-  w: '40px',
-  h: '40px',
-  lineHeight: '40px',
-  textAlign: 'center',
-  borderRadius: '10px',
-  border: '1px solid rgba(0, 0, 0, 0.8)',
-  fontSize: '18px',
-  color: 'blackAlpha.800',
+	w: "40px",
+	h: "40px",
+	lineHeight: "40px",
+	textAlign: "center",
+	borderRadius: "10px",
+	border: "1px solid rgba(0, 0, 0, 0.8)",
+	fontSize: "18px",
+	color: "blackAlpha.800",
 };
 
 export default top;
