e147995bee0253fc11da63f9d4af96a624806bcb
mai <amaomruhki@gmail.com>
2022-10-10 19:52:11 +0900

    #27_chore_Recoilの導入

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,16 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import { RecoilRoot } from "recoil";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
-  );
+	return (
+		<RecoilRoot>
+			<ChakraProvider>
+				<Component {...pageProps} />
+			</ChakraProvider>
+		</RecoilRoot>
+	);
 }
 
 export default MyApp;
