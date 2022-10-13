57bc42bdab1267ded3a00d632e1231e421b1a920
mai <amaomruhki@gmail.com>
2022-10-13 18:34:35 +0900

    #21_timestampをnull以外の時に処理するように調整

--- pages/detail.tsx
+++ pages/detail.tsx
@@ -52,18 +52,17 @@ const Detail = () => {
 	// const { id } = router.query;
 
 	// Timestamp型から文字列への変換
-	const parseTimestampToDate = (
-		timestamp: Timestamp,
-		separator: string
-	): string => {
-		const date = timestamp.toDate();
-		const year = date.getFullYear();
-		const month = ("00" + (date.getMonth() + 1)).slice(-2);
-		const day = ("00" + date.getDate()).slice(-2);
-		const hour = ("00" + date.getHours()).slice(-2);
-		const minutes = ("00" + date.getMinutes()).slice(-2);
+	const parseTimestampToDate = (timestamp: Timestamp, separator: string) => {
+		if (timestamp != null) {
+			const date = timestamp.toDate();
+			const year = date.getFullYear();
+			const month = ("00" + (date.getMonth() + 1)).slice(-2);
+			const day = ("00" + date.getDate()).slice(-2);
+			const hour = ("00" + date.getHours()).slice(-2);
+			const minutes = ("00" + date.getMinutes()).slice(-2);
 
-		return `${year}${separator}${month}${separator}${day} ${hour}:${minutes}`;
+			return `${year}${separator}${month}${separator}${day} ${hour}:${minutes}`;
+		}
 	};
 
 	// コメントの取得
